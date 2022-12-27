37a55338ee2358ea219379379abc1781450f3580
David <vasqued2@yahoo.com>
2022-12-26 19:44:06 -0500

    Changed CONFIG_DATA to match json file

--- tests/const.py
+++ tests/const.py
@@ -1,3 +1,3 @@
 """Constants for tests."""
 
-CONFIG_DATA = {"league_id": "NFL", "team_id": "SEA", "name": "team_tracker", "timeout": 120, "conference_id": "9999"}
+CONFIG_DATA = {"league_id": "MLB", "team_id": "MIA", "name": "test_tt_all_test01", "timeout": 120, "conference_id": "9999"}
