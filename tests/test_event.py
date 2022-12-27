23fb2247b71a266d90edaae194b9b84ab890ee71
David <vasqued2@yahoo.com>
2022-12-27 08:49:15 -0500

    Add call to async_process_event()

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,11 +1,13 @@
 """Test NFL Sensor"""
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.teamtracker.const import DOMAIN
+from custom_components.teamtracker.const import DOMAIN, DEFAULT_LOGO
 from tests.const import CONFIG_DATA
+from custom_components.teamtracker.clear_values import async_clear_values
+from custom_components.teamtracker.event import async_process_event
 
 
-async def test_sensor(hass):
+async def test_eventzxc(hass):
 
     entry = MockConfigEntry(
         domain=DOMAIN,
@@ -23,3 +25,36 @@ async def test_sensor(hass):
 
     assert state
     assert state.state == "PRE"
+
+#
+#  New test 
+#
+
+    values = await async_clear_values()
+    values["sport"] = "baseball"
+    values["league"] = "MLB""
+    values["league_logo"] = DEFAULT_LOGO
+    values["team_abbr"] = "MIA"
+    values["state"] = "NOT_FOUND"
+    values["last_update"] = "2022-12-27 08:32:48-05:00"
+    values["private_fast_refresh"] = False
+
+    async with aiofiles.open('tests/tt/all.json', mode='r') as f:
+        contents = await f.read()
+    data = json.loads(contents)
+
+    sensor_name = "test_sensor"
+    sport_path = values["sport"]
+    league_id = values["league"]
+    team_id = values["team_abbr"]
+    lang = "en"
+    url = "tests/tt/all.json"
+
+    if data is None:
+        values["api_message"] = "API error, no data returned"
+        _LOGGER.warn("%s: Error with test file '%s':  %s", sensor_name, team_id, "tests/tt/all.json")
+
+    _LOGGER.debug("%s: calling async_process_event()", sensor_name)
+    values = await async_process_event(values, sensor_name, data, sport_path, league_id, DEFAULT_LOGO, team_id, lang, url)
+
+    assert values
