bc8098be4c6aca30214f5f7aa39323d7cad763f8
David <vasqued2@yahoo.com>
2023-01-11 20:14:08 -0500

    getlocale()

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -13,7 +13,7 @@ async def test_sensor(hass, mocker):
         data=CONFIG_DATA,
     )
 
-    mocker.patch("locale.get_locale", return_value=("en", 0))
+    mocker.patch("locale.getlocale", return_value=("en", 0))
     entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
