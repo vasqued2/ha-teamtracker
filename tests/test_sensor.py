522028f79a63e0dad878b965e1b8b99306af1a5f
David <vasqued2@yahoo.com>
2023-01-14 06:52:51 -0500

    mock the aiofiles.open call

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,4 +1,8 @@
 """Test NFL Sensor"""
+import asyncio
+import aiohttp
+import aiofiles
+
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.teamtracker.const import DOMAIN
@@ -14,6 +18,12 @@ async def test_sensor(hass, mocker):
     )
 
     mocker.patch("locale.getlocale", return_value=("en", 0))
+
+    async with aiofiles.open('tests/tt/all.json', mode='r') as f:
+        contents = await f.read()
+    mocker.patch('aiofiles.open', return_value=mocker.mock_open(read_data=contents).return_value)
+
+
     entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
