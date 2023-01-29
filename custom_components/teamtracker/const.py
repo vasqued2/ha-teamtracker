591ac51e3c4bcffe2663a0e1e7a5e4bfa793afd9
David <vasqued2@yahoo.com>
2023-01-28 19:35:19 -0500

    Update to v0.7.0.  v0.6.x won't work after 2023.03

--- custom_components/teamtracker/const.py
+++ custom_components/teamtracker/const.py
@@ -77,7 +77,7 @@ DEFAULT_KICKOFF_IN = "{test} days"
 
 # Misc
 TEAM_ID = ""
-VERSION = "v0.6.4"
+VERSION = "v0.7.0"
 ISSUE_URL = "https://github.com/vasqued2/ha-teamtracker"
 DOMAIN = "teamtracker"
 PLATFORM = "sensor"
