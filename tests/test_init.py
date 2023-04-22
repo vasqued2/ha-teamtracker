d460d27b190a60a33e8b51006b84c216a3ead1e0
David <vasqued2@yahoo.com>
2023-04-22 06:27:20 -0800

    Clean up tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,29 +1,24 @@
-"""Tests for init."""
-#import pytest
+# Tests for init
 
+import pytest
 from pytest_homeassistant_custom_component.common import MockConfigEntry
-
 from custom_components.teamtracker.const import DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
-
 from tests.const import CONFIG_DATA
 
-
-#@pytest.fixture(autouse=True)
-#def expected_lingering_timers() -> bool:
-#    """Temporary ability to bypass test failures due to lingering timers.
-#    Parametrize to True to bypass the pytest failure.
-#    @pytest.mark.parametrize("expected_lingering_timers", [True])
-#    This should be removed when all lingering timers have been cleaned up.
-#    """
-#    return False
+# Temporary ability to bypass test failures due to lingering timers.
+#   Parametrize to True to bypass the pytest failure.
+#   @pytest.mark.parametrize("expected_lingering_timers", [True])
+#   This should be removed when all lingering timers have been cleaned up.
+@pytest.fixture(autouse=True)
+def expected_lingering_timers() -> bool:
+    return False
 
     
 #@pytest.mark.parametrize("expected_lingering_timers", [True])
 async def test_setup_entry(
     hass,
 ):
-    """Test settting up entities."""
     entry = MockConfigEntry(
         domain=DOMAIN,
         title="team_tracker",
@@ -44,7 +39,7 @@ async def test_setup_entry(
 
 #@pytest.mark.parametrize("expected_lingering_timers", [True])
 async def test_unload_entry(hass):
-    """Test unloading entities."""
+
     entry = MockConfigEntry(
         domain=DOMAIN,
         title="team_tracker",
--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,27 +1,21 @@
-"""Test NFL Sensor"""
+#  Test Team Tracker Sensor
 
-#import pytest
-
+import pytest
 from pytest_homeassistant_custom_component.common import MockConfigEntry
-
 from custom_components.teamtracker.const import DOMAIN
-
 from tests.const import CONFIG_DATA
 
-
-#@pytest.fixture(autouse=True)
-#def expected_lingering_timers() -> bool:
-#    """Temporary ability to bypass test failures due to lingering timers.
-#    Parametrize to True to bypass the pytest failure.
-#    @pytest.mark.parametrize("expected_lingering_timers", [True])
-#    This should be removed when all lingering timers have been cleaned up.
-#    """
-#    return False
+# Temporary ability to bypass test failures due to lingering timers.
+#   Parametrize to True to bypass the pytest failure.
+#   @pytest.mark.parametrize("expected_lingering_timers", [True])
+#   This should be removed when all lingering timers have been cleaned up.
+@pytest.fixture(autouse=True)
+def expected_lingering_timers() -> bool:
+    return False
 
     
 #@pytest.mark.parametrize("expected_lingering_timers", [True])
 async def test_sensor(hass, mocker):
-    """ Make sure sensor gets added """
 
     entry = MockConfigEntry(
         domain=DOMAIN,
@@ -30,9 +24,6 @@ async def test_sensor(hass, mocker):
     )
 
     mocker.patch("locale.getlocale", return_value=("en", 0))
-
-    #    contents = "{}"
-    #    mocker.patch('aiofiles.open', return_value=mocker.mock_open(read_data=contents).return_value)
 
     entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(entry.entry_id)
