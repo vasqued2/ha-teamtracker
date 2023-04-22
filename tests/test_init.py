d616197cc9919e9c87e2326eb5e8471d5809b582
David <vasqued2@yahoo.com>
2023-04-22 14:02:29 -0800

    Remove test

--- tests/test_init.py
+++ tests/test_init.py
@@ -4,7 +4,6 @@ import pytest
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 from custom_components.teamtracker.const import DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
-from homeassistant.helpers.entity_registry import async_get
 from homeassistant.setup import async_setup_component
 from tests.const import CONFIG_DATA
 from unittest.mock import patch
@@ -72,29 +71,3 @@ async def test_unload_entry(hass):
 
     assert await entry.async_unload(hass)
     await hass.async_block_till_done()
-
-
-async def test_import(hass):
-    """Test importing a config."""
-
-    entry = MockConfigEntry(
-        domain=DOMAIN,
-        title="team_tracker",
-        data=CONFIG_DATA,
-    )
-
-    await async_setup_component(hass, "persistent_notification", {})
-    with patch(
-        "custom_components.teamtracker.async_setup_entry",
-        return_value=True,
-    ) as mock_setup_entry:
-
-        ent_reg = async_get(hass)
-        ent_entry = ent_reg.async_get_or_create(
-            "sensor", DOMAIN, unique_id="replaceable_unique_id", config_entry=entry
-        )
-        entity_id = ent_entry.entity_id
-        entry.add_to_hass(hass)
-        await hass.config_entries.async_setup(entry.entry_id)
-        assert entry.unique_id is None
-#        assert ent_reg.async_get(entity_id).unique_id == entry.entry_id
