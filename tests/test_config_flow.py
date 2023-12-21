2dedfe3ae37e1b47d0e9b736b552201e65df4553
David <vasqued2@yahoo.com>
2023-12-21 12:33:50 -0900

    test 3

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -99,10 +99,7 @@ async def test_options_flow_init(hass):
     await hass.async_block_till_done()
 
     # show initial form
-    result = await hass.config_entries.options.async_init(config_entry.entry_id)
-
-    await hass.async_block_till_done()
-
+#    result = await hass.config_entries.options.async_init(config_entry.entry_id)
 
     assert "form" == result["type"]
     assert "init" == result["step_id"]
