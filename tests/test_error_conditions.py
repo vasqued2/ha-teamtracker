0487a8917fce792e078a5ade593e493a89d7b6c0
David <vasqued2@yahoo.com>
2024-08-03 07:39:42 -0800

    Add additional tests

--- tests/test_error_conditions.py
+++ tests/test_error_conditions.py
@@ -29,8 +29,22 @@ _LOGGER = logging.getLogger(__name__)
 async def test_error_conditions(hass):
     """ Use file w/ test json and loop through test cases and compare to expected results """
 
-    competition = await async_get_value({}, "competitions", 0)
-
+    rc = await async_set_baseball_values({}, {}, 0, 0, "sensor_name")
+    assert rc == False
+    rc = await async_set_cricket_values({}, {}, 0, 0, "sensor_name")
+    assert rc == False
+    rc = await async_set_golf_values({}, {}, 0, 0, "sensor_name")
+    assert rc == False
+    rc = await async_set_hockey_values({}, {}, 0, 0, "sensor_name")
+    assert rc == False
+    rc = await async_set_mma_values({}, {}, 0, 0, "sensor_name")
+    assert rc == False
+    rc = await async_set_racing_values({}, {}, 0, 0, "sensor_name")
+    assert rc == False
+    rc = await async_set_soccer_values({}, {}, 0, 0, "sensor_name")
+    assert rc == False
+    rc = await async_set_tennis_values({}, {}, 0, 0, "sensor_name")
+    assert rc == False
     rc = await async_set_volleyball_values({}, {}, 0, 0, "sensor_name")
     assert rc == False
 
