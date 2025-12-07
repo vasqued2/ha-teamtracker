21aab22dd5279a2db4a2b5a3d39336214fae6c86
David <vasqued2@yahoo.com>
2025-12-06 20:53:09 -0500

    Uncomment 1

--- tests/test_init.py
+++ tests/test_init.py
@@ -96,8 +96,8 @@ async def test_unload_entry(hass):
     )
 
     entry.add_to_hass(hass)
-#    assert await hass.config_entries.async_setup(entry.entry_id)
-#    await hass.async_block_till_done()
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
 
 #    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
 #    entries = hass.config_entries.async_entries(DOMAIN)
