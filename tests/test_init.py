97ed372e84ff678de6bbc6a4f36cb648546caeda
David <vasqued2@yahoo.com>
2025-12-06 22:07:38 -0500

    Revert test_init.py to original

--- tests/test_init.py
+++ tests/test_init.py
@@ -30,60 +30,58 @@ async def test_setup_entry(
         data=CONFIG_DATA,
     )
 
-#    entry.add_to_hass(hass)
-#    assert await hass.config_entries.async_setup(entry.entry_id)
-#    await hass.async_block_till_done()
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
 
-#    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
-#    entries = hass.config_entries.async_entries(DOMAIN)
-#    assert len(entries) == 1
+    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
 
 #
 # Validate sensor state and attributes based on CONFIG_DATA
 #
 
-#    sensor_state = hass.states.get("sensor.test_tt_all_test01")
+    sensor_state = hass.states.get("sensor.test_tt_all_test01")
 
-#    assert sensor_state.state == "PRE"
-#    team_abbr = sensor_state.attributes.get("team_abbr")
-#    assert team_abbr == "MIA"
-#    sport = sensor_state.attributes.get("sport")
-#    assert sport == "baseball"
+    assert sensor_state.state == "PRE"
+    team_abbr = sensor_state.attributes.get("team_abbr")
+    assert team_abbr == "MIA"
+    sport = sensor_state.attributes.get("sport")
+    assert sport == "baseball"
 
 
-#    await hass.services.async_call(
-#        domain="teamtracker",
-#        service="call_api",
-#        service_data={
-#            "sport_path": "basketball",
-#            "league_path": "nba",
-#            "team_id": "bos"
-#        },
-#        target={
-#            "entity_id": [
-#                "sensor.test_tt_all_test01",
-#            ]
-#        },
-#        blocking=True
-#    )
+    await hass.services.async_call(
+        domain="teamtracker",
+        service="call_api",
+        service_data={
+            "sport_path": "basketball",
+            "league_path": "nba",
+            "team_id": "bos"
+        },
+        target={
+            "entity_id": [
+                "sensor.test_tt_all_test01",
+            ]
+        },
+        blocking=True
+    )
 
 #
 # Validate sensor state and attributes changed based on API call
 #
 
-#    sensor_state = hass.states.get("sensor.test_tt_all_test01")
+    sensor_state = hass.states.get("sensor.test_tt_all_test01")
 
-#    assert sensor_state.state == "NOT_FOUND"
-#    team_abbr = sensor_state.attributes.get("team_abbr")
-#    assert team_abbr == "BOS"
-#    sport = sensor_state.attributes.get("sport")
-#    assert sport == "basketball"
+    assert sensor_state.state == "NOT_FOUND"
+    team_abbr = sensor_state.attributes.get("team_abbr")
+    assert team_abbr == "BOS"
+    sport = sensor_state.attributes.get("sport")
+    assert sport == "basketball"
 
 #    assert await entry.async_unload(hass)
 #    await hass.async_block_till_done()
 
-#    assert await hass.config_entries.async_unload(entry.entry_id)
-#    await hass.async_block_till_done()
 
 #@pytest.mark.parametrize("expected_lingering_timers", [True])
 async def test_unload_entry(hass):
@@ -99,19 +97,18 @@ async def test_unload_entry(hass):
     assert await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
 
-#    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
-#    entries = hass.config_entries.async_entries(DOMAIN)
-#    assert len(entries) == 1
+    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
+    entries = hass.config_entries.async_entries(DOMAIN)
+    assert len(entries) == 1
 
-#    assert await hass.config_entries.async_unload(entries[0].entry_id)
-#    await hass.async_block_till_done()
-#    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
-#    assert len(hass.states.async_entity_ids(DOMAIN)) == 0
+    assert await hass.config_entries.async_unload(entries[0].entry_id)
+    await hass.async_block_till_done()
+    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 1
+    assert len(hass.states.async_entity_ids(DOMAIN)) == 0
 
-#    assert await hass.config_entries.async_remove(entries[0].entry_id)
-#    await hass.async_block_till_done()
-#    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 0
+    assert await hass.config_entries.async_remove(entries[0].entry_id)
+    await hass.async_block_till_done()
+    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 0
 
-#    assert await entry.async_unload(hass) # old version
-    await hass.config_entries.async_unload(entry.entry_id) # new version
+    assert await entry.async_unload(hass)
     await hass.async_block_till_done()
