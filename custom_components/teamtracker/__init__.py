1ab4ce6241e9a7dc2b297d467c034fdc848c2479
Wayward <rohn2024@gmail.com>
2026-04-27 20:50:50 -0400

    Add PWHL Support (#290)
    * Add support for PWHL
    
    Implement hockeytech API using a function which translates hockeytech
    API calls to ESPN's format.
    
    PWHL added to translation JSON files
    
    * Trigger workflow test
    
    * Workflow test
    
    * Add PWHL info to README
    
    ---------
    
    Co-authored-by: Wayward <adigitaldruid@proton.me>
    Co-authored-by: David <vasqued2@yahoo.com>

--- custom_components/teamtracker/__init__.py
+++ custom_components/teamtracker/__init__.py
@@ -38,6 +38,7 @@ from .const import (
     DEFAULT_LOGO,
     DEFAULT_TIMEOUT,
     DOMAIN,
+    HOCKEYTECH_LEAGUES,
     ISSUE_URL,
     LEAGUE_MAP,
     PLATFORMS,
@@ -50,6 +51,7 @@ from .const import (
     VERSION,
 )
 from .event import async_process_event
+from .hockeytech import async_fetch_hockeytech_scoreboard
 from .utils import is_integer, async_call_espn_api, async_get_value, has_team
 
 _LOGGER = logging.getLogger(__name__)
