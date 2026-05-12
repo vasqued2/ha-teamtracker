c2f08f8f4c990f51ad09bf276bbb78604b090eda
David Vasquez <vasqued2@yahoo.com>
2026-05-11 21:48:24 -0400

    Enable file override for local live testing

--- custom_components/teamtracker/espn.py
+++ custom_components/teamtracker/espn.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import aiofiles
 import aiohttp
+import os
 from datetime import date, timedelta
 import json
 import logging
@@ -239,6 +241,11 @@ class EspnProvider(BaseSportProvider):
             url,
         )
 
+        if file_override:
+            data = await self.async_override_espn_api(sensor_name, team_id, base_url)
+            return {"data": data, "url": url}
+
+
         headers = {"User-Agent": USER_AGENT, "Accept": "application/ld+json"}
         session = async_get_clientsession(hass)
         try:
@@ -259,3 +266,55 @@ class EspnProvider(BaseSportProvider):
             return {"data": None, "url": url}
 
         return {"data": data, "url": url}
+
+
+    #
+    #  Call an ESPN API (or file use the appropriate file override) and get the data returned by it
+    #    This utility will eventually replace/wrap all API calls
+    #
+    async def async_override_espn_api(self, sensor_name, team_id, url) -> dict | None:
+        """Read a json file to mock the ESPN API."""
+
+        _LOGGER.debug("%s: Overriding API for '%s'", sensor_name, team_id)
+
+        if sensor_name == "api_error":
+            return None
+
+        clean_url = url.split('?')[0]
+
+        _LOGGER.debug("%s: Overriding ESPN API (%s) for '%s'", sensor_name, url, team_id)
+        if "schedule" in clean_url:
+            file_path = "/share/tt/schedule.json"
+            if not os.path.exists(file_path):
+                file_path = "tests/tt/schedule.json"
+        elif "teams" in clean_url:
+            if clean_url[-1].isdigit(): # if there is any team identifier, use team 194
+                file_path = "/share/tt/teams-194.json"
+                if not os.path.exists(file_path):
+                    file_path = "tests/tt/teams-194.json"
+            elif "football" in clean_url:
+                file_path = "/share/tt/teams-ncaaf-small.json"
+                if not os.path.exists(file_path):
+                    file_path = "tests/tt/teams-ncaaf-small.json"
+            else:
+                file_path = "/share/tt/teams.json"
+                if not os.path.exists(file_path):
+                    file_path = "tests/tt/team.json"
+        elif "/all/" in clean_url:
+            file_path = "/share/tt/scoreboard_all_leagues.json"
+            if not os.path.exists(file_path):
+                file_path = "tests/tt/scoreboard_all_leagues.json"
+        else:
+            file_path = "/share/tt/all.json"
+            if not os.path.exists(file_path):
+                file_path = "tests/tt/all.json"
+
+        try:
+            async with aiofiles.open(file_path, mode="r") as f:
+                contents = await f.read()
+            data = json.loads(contents)
+        except Exception as e: # pylint: disable=broad-exception-caught
+            _LOGGER.debug("%s: API file read failed: %s", sensor_name, e)
+            data = None
+
+        return(data)
